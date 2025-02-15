ea753b04eaab4f8d2baf377b71df9ba12ca0bf03
Alexandru Craevschi <54686175+acraevschi@users.noreply.github.com>
2025-02-15 21:29:59 +0100

    update comments for cities skylines

--- models/rl_model.py
+++ models/rl_model.py
@@ -2,5 +2,4 @@ from keys.actions import perform_action
 
 # receive input from CNN, output action
 # call perform_action(action) to perform the action
-# continuously check health bar (make a screenshot of the lower part of the screen, filter red pixels, calculate the percentage of red pixels)
-# calculate reward/penalty given the action and health bar
+# continuously check population size and economy
